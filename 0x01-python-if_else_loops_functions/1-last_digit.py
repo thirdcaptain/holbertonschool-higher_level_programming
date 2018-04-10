e413ee37339cb8f9610faec9c8bf0304fc910406
Isaac Wong <isaac@thirdcaptain.com>
2018-04-10 23:23:19 +0000

    Format specifiers used

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ if number < 0:
 else:
     lastdigit = number % 10
 if lastdigit > 5:
-    print('Last digit of %d is %d and is greater than 5'
-          % (number, lastdigit))
+    print('Last digit of {} is {} and is greater than 5'
+          .format(number, lastdigit))
 elif lastdigit == 0:
-    print('Last digit of %d is %d and is 0' % (number, lastdigit))
+    print('Last digit of {} is {} and is 0'.format(number, lastdigit))
 elif lastdigit < 6:
-    print('Last digit of %d is %d and is less than 6 and not 0'
-          % (number, lastdigit))
+    print('Last digit of {} is {} and is less than 6 and not 0'
+          .format(number, lastdigit))
