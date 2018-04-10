0d287eea1a4316bca21b3f3f0762fbe88aaacdb9
Isaac Wong <isaac@thirdcaptain.com>
2018-04-10 03:51:47 +0000

    Initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a cycle exists
+ * @list: pointer to head of list
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	slow = list;
+	fast = list;
+
+	while (fast != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next;
+		fast = fast->next;
+		if (slow == fast)
+			return (1);
+
+	}
+
+	return (0);
+}
