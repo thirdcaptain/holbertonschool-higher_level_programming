e75aad1f126e9f68478afc7be1aae377bcd117f3
Isaac Wong <isaac@thirdcaptain.com>
2018-04-12 23:28:31 +0000

    Added single number case

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 3:
+    if len(sys.argv) == 1:
         print("0")
+    elif len(sys.argv) == 2:
+        num1 = int(sys.argv[1])
+        print("{}".format(num1))
     else:
         num1 = int(sys.argv[1])
         num2 = int(sys.argv[2])
