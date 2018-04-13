c96e7c76c075bb3ec93bf2da95dbcb2c2b5c144f
Isaac Wong <isaac@thirdcaptain.com>
2018-04-13 00:13:09 +0000

    Accounted for infinite arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("0")
-    elif len(sys.argv) == 2:
-        num1 = int(sys.argv[1])
-        print("{}".format(num1))
     else:
-        num1 = int(sys.argv[1])
-        num2 = int(sys.argv[2])
-        print("{}".format(num1 + num2))
+        i = 1
+        sum = 0
+        while i < (len(sys.argv)):
+            sum = sum + int(sys.argv[i])
+            i += 1
+        print("{}".format(sum))
