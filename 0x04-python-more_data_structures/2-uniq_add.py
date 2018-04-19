e98d5d4017b746d85f6b2c2407c63102c278cee4
Isaac Wong <isaac@thirdcaptain.com>
2018-04-19 23:08:26 +0000

    Used sets

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+#def uniq_add(my_list=[]):
+#    sum = 0
+#    for element in my_list:
+#        count = my_list.count(element)
+#        if count > 1:
+#            for i in range(count - 1):
+#                my_list.remove(element)
+#    for element in my_list:
+#        sum += element
+#    return sum
 def uniq_add(my_list=[]):
+    set_list = set(my_list)
     sum = 0
-    for element in my_list:
-        count = my_list.count(element)
-        if count > 1:
-            for i in range(count - 1):
-                my_list.remove(element)
-    for element in my_list:
+    print (set_list)
+    for element in set_list:
         sum += element
     return sum
