9d651ae7c50db782357dca431b87493bd3fb8f56
Isaac Wong <isaac@thirdcaptain.com>
2018-04-19 21:45:35 +0000

    Removed debugging statements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,14 +2,10 @@
 def uniq_add(my_list=[]):
     sum = 0
     for element in my_list:
-        print(my_list)
-        print(element)
         count = my_list.count(element)
         if count > 1:
             for i in range(count - 1):
-                print(my_list)
                 my_list.remove(element)
-    print(my_list)
     for element in my_list:
         sum += element
     return sum
