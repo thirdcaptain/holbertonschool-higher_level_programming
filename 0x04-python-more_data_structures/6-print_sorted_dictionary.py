d082f6a510b947e7099e6347e4114825181d2b11
Isaac Wong <isaac@thirdcaptain.com>
2018-04-20 03:59:54 +0000

    Style fixes

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary)
     for i in sorted_dict:
-        print("{country}: {capital}".format(country=i, capital=a_dictionary[i]))
+        print("{name}: {val}".format(name=i, val=a_dictionary[i]))
