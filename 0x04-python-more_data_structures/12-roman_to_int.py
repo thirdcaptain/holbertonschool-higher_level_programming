6742775f6f4ea5943654ac6a6f3eb170a389e03e
Isaac Wong <isaac@thirdcaptain.com>
2018-04-21 00:02:51 +0000

    Added subtractive logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,13 @@ def roman_to_int(roman_string):
     num = 0
     list = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string:
-        for i in reversed(roman_string):
+        prev = 0
+        for i in roman_string:
             if i in list:
+                if list[i] > prev:
+                    num -= prev * 2
                 num += list[i]
+                prev = list[i]
             else:
                 return 0
         return num
