76b9aa7f1c683385641141f02d156d4d4a43ab43
Isaac Wong <isaac@thirdcaptain.com>
2018-05-01 00:41:09 +0000

    Shebang added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0;
-    sum = 0;
+    count = 0
+    sum = 0
     try:
         for element in range(x):
             print("{}".format(my_list[element]), end="")
             count += 1
     except:
-        for element in range(my_list):
-            sum += 1
+        pass
     finally:
         print()
         return count
