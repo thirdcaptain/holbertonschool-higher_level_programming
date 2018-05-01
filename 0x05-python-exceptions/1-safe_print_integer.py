cd5c9399ad5542085aafcd947d5423474e0183cc
Isaac Wong <isaac@thirdcaptain.com>
2018-05-01 01:12:31 +0000

    Variable name corrected

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-#        number = int(value)
-        print("{:d}".format(number))
+        print("{:d}".format(value))
         return True
     except:
         return False
