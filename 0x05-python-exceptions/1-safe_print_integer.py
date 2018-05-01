9a1b0f5fbb695c621109dcafd60cd98a30290f4e
Isaac Wong <isaac@thirdcaptain.com>
2018-05-01 01:10:14 +0000

    Removed cast

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        number = int(value)
+#        number = int(value)
         print("{:d}".format(number))
         return True
     except:
