55ba44d13e3ae9bfd845d805f66cc46297ca578e
Isaac Wong <isaac@thirdcaptain.com>
2018-05-01 00:46:00 +0000

    Different logic implemented

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    sum = 0
-    try:
-        for element in range(x):
-            print("{}".format(my_list[element]), end="")
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-    except:
-        pass
-    finally:
-        print()
-        return count
+        except:
+            print()
+            return count
+    print()
+    return count
+
+#    count = 0
+#    sum = 0
+#    try:
+#        for element in range(x):
+#            print("{}".format(my_list[element]), end="")
+#            count += 1
+#    except:
+#        pass
+#    finally:
+#        print()
+#        return count
