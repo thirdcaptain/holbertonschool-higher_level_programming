fa6d06c5f61e63e33de32816b99874beedab0ed9
Isaac Wong <isaac@thirdcaptain.com>
2018-05-01 00:58:43 +0000

    Initial commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        number = int(value)
+        print("{:d}".format(number))
+        return True
+    except:
+        return False
