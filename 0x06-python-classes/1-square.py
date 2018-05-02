530fb31d670fae9fa15bac43807d7e5493d70333
Isaac Wong <isaac@thirdcaptain.com>
2018-05-02 02:53:01 +0000

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a square with a private instance attribute
+"""
+
+class Square:
+    """
+    This class defines Square with attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
