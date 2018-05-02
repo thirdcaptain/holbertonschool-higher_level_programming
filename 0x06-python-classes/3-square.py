64fb4dc500a2f2f934ec62713d0d9bbc41b3347c
Isaac Wong <isaac@thirdcaptain.com>
2018-05-02 04:20:31 +0000

    Docstring fixes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a square with errors"""
+
+
+class Square:
+    """This class defines a square with attribute size with exceptions"""
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This method returns the area"""
+        return self.__size * self.__size
