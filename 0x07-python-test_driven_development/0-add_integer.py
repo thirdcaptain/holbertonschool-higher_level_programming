095b346d38ad0a2b679127cbc39ecb5dc292ae40
Isaac Wong <isaac@thirdcaptain.com>
2018-05-05 03:04:12 +0000

    Initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a method that sum of two integers"""
+
+
+def add_integer(a, b=98):
+    """Adds the sum of two integers"""
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    if a is None:
+        raise TypeError("a must be an integer")
+    if b is None:
+        raise TypeError("b must be an integer")
+    return a + b
