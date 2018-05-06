69268487ac03dcbbf6ea658fc730c07c2b0b3cdd
Isaac Wong <isaac@thirdcaptain.com>
2018-05-06 04:40:55 +0000

    Matrix cases added

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
-"""Defines a function that divides a matrix by a num"""
+"""
+Defines a function that divides a matrix by a num
+"""
+
 
 def matrix_divided(matrix, div):
-    """Divides a matrix by a constant"""
+    """
+    Divides a matrix by a constant
+    """
+    if not isinstance(div, float) and not isinstance(div, int):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     new_matrix = []
     length = len(matrix)
-
+    """validate matrix"""
+    """validate matrix is made of lists"""
+    for i in range(length):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a matrix"
+                            " (list of lists) of integers/floats")
+    """verify element types in matrix """
+    for i in range(length):
+        for element in matrix[i]:
+            if not isinstance(element, float) and not isinstance(element, int):
+                raise TypeError("matrix must be a matrix"
+                                " (list of lists) of integers/floats")
     for i in range(length):
         new_matrix.append([])
         for element in matrix[i]:
             new_matrix[i].append(round((element / div), 2))
-
     return new_matrix
