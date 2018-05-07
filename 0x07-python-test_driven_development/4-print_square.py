8c0c2bcdda88ec8d112a919508450bc379ee6ade
Isaac Wong <isaac@thirdcaptain.com>
2018-05-07 05:36:46 +0000

    Changed 0 condition

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print()
         return
     for row in range(size):
         for column in range(size):
