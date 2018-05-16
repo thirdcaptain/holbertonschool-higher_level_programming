5872d59aeeaad72eee3cdd938274ee76184b19b9
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 05:17:29 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module defines is_same_class function that returns True if object is a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True returns True if object is an instance of a class
+    False if otherwise
+    """
+    if isinstance(object, a_class):
+        return True
+    else:
+        return False
