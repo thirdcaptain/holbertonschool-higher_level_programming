4facf5059e4ab9dafe223417dc48ea27c78768e1
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 05:55:02 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class defines baseGeometry
+    """
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
