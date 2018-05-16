ebdc4a769ab457389f13a8c0c0886ee94cc16494
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 22:42:25 +0000

    Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module defines read_file
+"""
+
+
+def read_file(filename=""):
+    """ reads file from text & prints to stdout
+    args:
+        filename (file): file to read
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
