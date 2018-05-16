43086fb42881e91d42b18b0e483ca61b24d218e3
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 05:31:08 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module defines is_same_class function that returns True if object is a sub/class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ determines if a obj is an instance of a class that is inherited
+    from a_class
+    Args:
+        obj (object): object to evaluate
+        a_class(class): class to evaluate
+
+    Returns:
+        True returns True if object/inherited object is an instance of a_class
+        False if otherwise
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
