5ec3aff4e4be91726dad5c07bb02c2b5f716e3a4
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 23:20:12 +0000

    Used read()

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename=""):
         filename (file): file to read
     """
     with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end="")
+        print(f.read())
