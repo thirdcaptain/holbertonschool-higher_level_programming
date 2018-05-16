9fd973cf0271fb717f20cf27dc00c003ecb36a87
Isaac Wong <isaac@thirdcaptain.com>
2018-05-16 04:36:06 +0000

    Added docstrings

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module defines lookup which returns attributes of an object
+"""
 
 def lookup(obj):
+    """ returns attributes of an object
+    parameters:
+        obj (obj): object to determine parameters
+    """
     return dir(obj)
