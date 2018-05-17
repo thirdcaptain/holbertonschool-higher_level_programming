ab5a8f78262bc02746b4d1ce77545741f4d673a5
Isaac Wong <isaac@thirdcaptain.com>
2018-05-17 01:35:49 +0000

    Initial commit

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module defines from_json_string function
+"""
+
+
+import json
+def from_json_string(my_obj):
+    """
+    returns python object represented by JSON string
+    args:
+        my_obj (obj): json object
+    """
+    return json.loads(my_obj)
