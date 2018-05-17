e29f3c50e8ec9d29b8b34826670d706836e8aed9
Isaac Wong <isaac@thirdcaptain.com>
2018-05-17 04:37:15 +0000

    Append to end of file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,6 +9,8 @@ from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-json_list = list(argv[1:])
 filename = "add_item.json"
-save_to_json_file(json_list, filename)
+json_list = argv[1:]
+my_obj = load_from_json_file(filename)
+my_obj += json_list
+save_to_json_file(my_obj, filename)
