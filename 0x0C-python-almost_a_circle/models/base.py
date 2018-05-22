16032ee40a52a2ac12047bc00bcaff711bde1b37
Isaac Wong <339@holbertonschool.com>
2018-05-22 19:15:55 +0000

    First JSON push

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,11 +30,19 @@ class Base:
         args:
             list_dictionaries (list): list of dictionaries
         """
-        if not any(list_dictionaries):
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        
+        list_objects = []
+        filename = str(cls.__name__) + ".json"
+        if not any(list_objs):
+            with open(filename, mode="w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(list_objects))
+        else:
+            for obj in list_objs:
+                list_objects.append(cls.to_dictionary(obj))
+            with open(filename, mode="w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(list_objects))
