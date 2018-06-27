de34d3de15f3e7a3c550223362ed504ca7930c09
Isaac Wong <isaac@thirdcaptain.com>
2018-06-27 05:32:53 +0000

    Removed extra comments

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,18 +5,3 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_shows DESC, genre ASC;
-
-/*
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
-*/
-/*
-SELECT tv_shows.title, tv_show_genres.show_id
-FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
-*/
