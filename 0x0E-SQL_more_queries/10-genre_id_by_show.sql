e64f279468b9ec51335c173c06c46c51dd7fcff8
Isaac Wong <isaac@thirdcaptain.com>
2018-06-27 04:20:27 +0000

    Changed from left join to inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
+INNER JOIN tv_show_genres -- changed to inner join changed to exclude NULL
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
