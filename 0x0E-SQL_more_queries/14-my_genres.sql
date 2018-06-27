51179991f5f78d57cfe0f6a2d504f1918ff20668
Isaac Wong <isaac@thirdcaptain.com>
2018-06-27 05:59:05 +0000

    Removed extra comments with SELECT

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,18 +6,3 @@ ON tv_genres.id = tv_show_genres.genre_id
 LEFT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-
-/*
-SELECT tv_shows.title, tv_shows.id
-FROM tv_shows
-WHERE title='Dexter'
-*/
-
-/*
-SELECT tv_genres.name AS genre, COUNT(*)  AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY number_shows DESC, genre ASC;
-*/
