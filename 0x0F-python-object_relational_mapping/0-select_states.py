e8733e65a145b487f9abca46a680e5741b119097
Isaac Wong <isaac@thirdcaptain.com>
2018-07-20 02:31:24 +0000

    Arguments parameters added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,16 @@
 
 if __name__=="__main__":
     import MySQLdb
+    from sys import argv
 
+    # default values user="root", passwd="", db="hbtn_0e_0_usa"
     db = MySQLdb.connect(host="localhost",
-                         user="root",
-                         passwd="",
-                         db="hbtn_0e_0_usa")
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     for row in cur.fetchall():
         print(row)
