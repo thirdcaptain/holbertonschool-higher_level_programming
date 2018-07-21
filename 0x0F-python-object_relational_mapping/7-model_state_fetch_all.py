853ffd668164b155c656d61b492a9052fec3bc56
Isaac Wong <isaac@thirdcaptain.com>
2018-07-21 06:02:33 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""module lists all State objects from database hbtn_0e_6_usa using SQLAlchemy
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import inspect
+
+if __name__ == "__main__":
+    username = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    # setup engine
+    engine_string = "mysql://{}:{}@localhost:3306/{}".format(username,
+                                                             passwd, db)
+    # default username: root, passwd: "", db: hbtn_0e_6_usa
+    engine = create_engine(engine_string)
+    Base.metadata.bind = engine
+
+    # setup session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    list = session.query(State).all()
+    if not list:
+        print("Nothing")
+    else:
+        for elem in list:
+            print("{}: {}".format(elem.id, elem.name))
