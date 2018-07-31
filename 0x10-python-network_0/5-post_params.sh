8079b062e21e9c598ece187b82ef6815d89af6ce
Isaac Wong <isaac@thirdcaptain.com>
2018-07-31 22:02:58 +0000

    Formatting changes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X "POST" "$1" -sd "subject=I will always be here for PLD&email=hr@holbertonschool"
+curl -X "POST" "$1" -sd "subject=I will always be here for PLD&email=hr@holbertonschool.com"
