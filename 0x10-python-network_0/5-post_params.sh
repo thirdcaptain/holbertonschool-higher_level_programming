4780b5278ec95b0436a425247b7a0c0ac3bda8f1
Isaac Wong <isaac@thirdcaptain.com>
2018-07-31 21:59:57 +0000

    Initial commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X "POST" "$1" -sd "email:hr@holbertonschool & subject: I will always be here for PLD"
+curl -X "POST" "$1" -sd "subject=I will always be here for PLD&email=hr@holbertonschool"
