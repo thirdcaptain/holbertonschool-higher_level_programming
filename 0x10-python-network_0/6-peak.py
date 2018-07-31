a9e3d057a93420799c30948534c0f4f2f20f4db8
Isaac Wong <Isaac Wong>
2018-07-31 23:55:35 +0000

    Accounted for 1 and 2 numbers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,19 +6,29 @@ def find_peak(list_of_integers):
     """returns a peak in a list"""
     if list_of_integers:
         # half = len(list_of_integers) // 2
+        length = len(list_of_integers)
         for num in range(len(list_of_integers)):
             peak = list_of_integers[num]
-            if num == 0 and len(list_of_integers) > 1:
+            if length == 1:
+                return peak
+            if num == 0 and length == 2:
+                if peak > list_of_integers[num + 1]:
+                    return peak
+                elif peak < list_of_integers[num + 1]:
+                    return list_of_integers[num + 1]
+                elif peak == list_of_integers[num + 1]:
+                    return peak
+            if num == 0 and length > 1:
                 if list_of_integers[num + 1] < peak:
                     return peak
-            elif num != len(list_of_integers):
+            elif num != length and length > 2:
                 if (list_of_integers[num - 1] < peak and
                         list_of_integers[num + 1] < peak):
                     return peak
                 if (list_of_integers[num - 1] == peak and
                         list_of_integers[num + 1] == peak):
                     return peak
-            elif num == len(list_of_integers):
+            elif num == length:
                 if (list_of_integers[num - 1] < peak):
                     return peak
     else:
