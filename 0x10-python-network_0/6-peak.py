477c83162c5b32dbd148105440649fb6c19cd967
Isaac Wong <Isaac Wong>
2018-07-31 23:28:19 +0000

    Initial commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,17 @@ def find_peak(list_of_integers):
         for num in range(len(list_of_integers)):
             peak = list_of_integers[num]
             if num == 0 and len(list_of_integers) > 1:
-                if list_of_integers[num + 1] < list_of_integers[num]:
-                    return list_of_integers[num]
-        return 5
+                if list_of_integers[num + 1] < peak:
+                    return peak
+            elif num != len(list_of_integers):
+                if (list_of_integers[num - 1] < peak and
+                    list_of_integers[num + 1] < peak):
+                    return peak
+                if (list_of_integers[num - 1] == peak and
+                    list_of_integers[num + 1] == peak):
+                    return peak
+            elif num == len(list_of_integers):
+                if (list_of_integers[num - 1] < peak):
+                    return peak
     else:
         return None
