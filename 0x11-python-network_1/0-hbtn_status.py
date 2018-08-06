9f4715f4a75c0e5855c1f3e3c4a4c127ea7f62d9
Isaac Wong <isaac@thirdcaptain.com>
2018-08-06 22:55:01 +0000

    __main__ guard added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """module fetches https://intranet.hbtn.io/status"""
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-html2 = html.decode("utf-8")
-print("Body response:")
-print("    - type: {}".format(type(html)))
-print("    - content: {}".format(html))
-print("    - utf8 content: {}".format(html2))
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html2 = html.decode("utf-8")
+        print("Body response:")
+        print("    - type: {}".format(type(html)))
+        print("    - content: {}".format(html))
+        print("    - utf8 content: {}".format(html2))
