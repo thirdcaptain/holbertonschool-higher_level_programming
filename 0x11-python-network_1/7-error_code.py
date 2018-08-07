a3bdcf14bee543b6ff6f8258657fb434a813d9c3
Isaac Wong <isaac@thirdcaptain.com>
2018-08-07 03:23:49 +0000

    Initial commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Requests a URL with a POST request and email parameter"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    page = requests.get(url)
+    if page.status_code == requests.codes.ok:
+        print(page.text)
+    else:
+        print("Error code: {}".format(page.status_code))
