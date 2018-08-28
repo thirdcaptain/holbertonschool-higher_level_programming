c160b4902935e6f3cc3a0b41195cf9655c67f127
Isaac Wong <Isaac Wong>
2018-08-28 23:19:25 +0000

    Initial commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVarOne = 'C is fun';
+const myVarTwo = 'Python is cool';
+const myVarThree = 'Javascript is amazing';
+const strArray = [myVarOne, myVarTwo, myVarThree];
+for (let i = 0; i < strArray.length; i++) {
+  console.log(strArray[i]);
+}
