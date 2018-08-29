4904816a04cd61e6c76d36dc586fac4ec9d02c0b
Isaac Wong <isaac@thirdcaptain.com>
2018-08-29 01:14:53 +0000

    Initial commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Prints the addition of 2 integers */
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(firstArg, secondArg));
