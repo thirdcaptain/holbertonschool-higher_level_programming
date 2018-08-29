b506e454bc1b12a19c8af704a89389d46b4de21f
Isaac Wong <isaac@thirdcaptain.com>
2018-08-29 00:38:17 +0000

    Initial commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Prints three strings */
+const myVarOne = 'C is fun';
+const myVarTwo = 'Python is cool';
+const myVarThree = 'Javascript is amazing';
+const strArray = [myVarOne, myVarTwo, myVarThree];
+for (let i = 0; i < strArray.length; i++) {
+  console.log(strArray[i]);
+}
