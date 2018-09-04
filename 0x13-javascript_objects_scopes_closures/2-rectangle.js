967147d19c4082628d8b1e1d266e6d1a7cabbcb6
Isaac Wong <isaac@thirdcaptain.com>
2018-09-04 20:59:06 +0000

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Empty rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
