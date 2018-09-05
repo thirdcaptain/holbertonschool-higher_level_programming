ea59c75f399674151bd99975c1f1034567c7251e
Isaac Wong <isaac@thirdcaptain.com>
2018-09-05 00:53:11 +0000

    Initial commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] == searchElement) {
+    if (list[i] === searchElement) {
       counter++;
     }
   }
-  return (counter)
-}
+  return (counter);
+};
