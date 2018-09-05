5fb19a505bd97ba76a085fb9db8c7ade5bbd17e0
Isaac Wong <isaac@thirdcaptain.com>
2018-09-05 00:32:18 +0000

    Amend commit

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Function that returns the number of occurences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] == searchElement) {
+      counter++;
+    }
+  }
+  return (counter)
+}
