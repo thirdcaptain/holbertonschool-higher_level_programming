4f08d1a6c3b4b19c2ae7b59815e1cbf5cd1f9915
Isaac Wong <339@holbertonschool.com>
2018-09-06 02:00:02 +0000

    Initial commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Displays the number of movies where Wedge Angilles is present
+
+let count = 0;
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let films = JSON.parse(body).results;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j] === 'https://swapi.co/api/people/18/') {
+          count++;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
